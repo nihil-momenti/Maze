63626132a068f3cc78ae9678db4b73d094342015
Wim Looman <ghostunderscore@gmail.com>
2010-10-09 16:10:29 +1300

    Specials are existing, need to give them models and stuff.

--- special.py
+++ special.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 
+#from Model import model
+
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
 class Special(object):
-  def __init__(self, location):
+  @classmethod
+  def init(cls, config):
+    cls.models = []
+    for model in config['models']:
+      cls.models.append(Model(model))
+    
+  def __init__(self, location, config):
     self.x = location[0]
     self.z = location[1]
 
@@ -14,6 +22,7 @@ class Special(object):
     self.listID = glGenLists(1); glNewList(self.listID, GL_COMPILE_AND_EXECUTE)
     glPushMatrix()
     glTranslate(self.x, 0, self.z)
+    #random.choice(Special.models).display()
     glutSolidCube(10)
     glPopMatrix()
     glEndList()
